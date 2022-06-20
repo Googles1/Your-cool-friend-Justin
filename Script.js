0b43fc5e1b128fce4d6b6caa3045135b7d990737
Googles1 <107815561+Googles1@users.noreply.github.com>
2022-06-19 20:55:08 -0500

    A

---
+++ Script.js
@@ -0,0 +1,9 @@
+const openBtn = document.getElementById ('open') 
+const closeBtn=document.getElementById('close');
+openBtn.addEventListener('click',function(){
+// Code run upon'open'click
+window.open ('https://www.youtube.com/watch?v=mCdA4bJAGGk&ab_channel=sweetblue')
+})
+closeBtn.addEventListener('click',function(){
+// Code run upon'close'click
+})
